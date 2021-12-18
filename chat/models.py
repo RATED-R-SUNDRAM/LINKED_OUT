c23d8ab0156de697c3f54e501e515e39da3a0162
Shivam Yadav <shivamyadav@sharechat.co>
2021-12-18 20:21:01 +0530

    django file update: added docstrings

--- chat/models.py
+++ chat/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-from datetime import datetime
-from home.models import userDetails
+from home.models import UserDetails
 
 # Create your models here.
 class Room(models.Model):
-    name = models.CharField(primary_key=True,max_length=1000)
+    name = models.CharField(primary_key=True, max_length=1000)
+
+
 class Message(models.Model):
-    sno=models.AutoField(primary_key=True)
+    sno = models.AutoField(primary_key=True)
     value = models.CharField(max_length=1000000)
     date = models.DateTimeField(auto_now_add=True, blank=True)
-    user = models.ForeignKey(userDetails,on_delete=models.CASCADE)
+    user = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
     room = models.CharField(max_length=1000)
-    name=models.CharField(max_length=1000,blank=True)
+    name = models.CharField(max_length=1000, blank=True)
